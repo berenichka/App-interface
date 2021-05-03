574801c641222b2fc82898d05b4c1929c5538efa
Veronika <pozhiloy@gmail.com>
2021-05-03 19:52:10 +0600

    HW1.2_app-interface - Project added

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Veronika Torushko on 01.05.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var statusText: UITextField!
+    
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var birthday: UILabel!
+    
+    @IBOutlet weak var city: UILabel!
+    
+    
+}
